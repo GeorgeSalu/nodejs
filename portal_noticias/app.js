86248751e3f3aa2e687ad668c05e44d6b9d6a01b
George Salu <george.salu10@gmail.com>
2018-01-18 22:00:09 -0200

    inclusao do ejs no projeto

--- portal_noticias/app.js
+++ portal_noticias/app.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine','ejs');
+
 app.get('/',function(req, res) {
-	res.send("<html><body>Portal de Noticias</body></html>")
+	res.send("<html><body>Portal de Noticias</body></html>");
 });
 
 app.get('/tecnologia', function(req, res) {
-	res.send("<html><body>Noticias de Tecnologia</body></html>")
+	res.render("secao/tecnologia");
 });
 
 app.listen(3000, function() {
