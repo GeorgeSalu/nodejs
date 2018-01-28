13a3990673fade9ff8c4b60fe8d65ea3be9a195a
George Salu <george.salu10@gmail.com>
2018-01-28 12:24:56 -0200

    implementado a logica da home

--- portal_noticias/app/models/NoticiasDAO.js
+++ portal_noticias/app/models/NoticiasDAO.js
@@ -14,6 +14,11 @@ NoticiasDAO.prototype.salvarNoticia = function(noticia, connection, callback) {
     this._connection.query('insert into noticias set ? ', noticia, callback);
 }
 
+NoticiasDAO.prototype.get5UltimasNoticias = function(callback) {
+    this._connection.query('select * from noticias order by data_criacao desc limit 5', callback);
+}
+
+
 module.exports = function() {
     return NoticiasDAO;
 }
