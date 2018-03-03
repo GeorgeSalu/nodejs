177bc80874d54c2390b5f86935b325f425dfbd7e
George Salu <george.salu10@gmail.com>
2018-03-02 23:00:17 -0300

    removendo comentarios 2

--- instagram_clone/api/server.js
+++ instagram_clone/api/server.js
@@ -153,12 +153,17 @@ app.put('/api/:id', function(req, res){
 
 // DELETE by ID(delete)
 app.delete('/api/:id', function(req, res){
-    res.send(req.params.id);
-    /*
     db.open(function(err, mongoclient){
         mongoclient.collection('postagens', function(err, collection){
-            collection.remove(
-                { _id : objectId(req.params.id) },
+            collection.update(
+                { },
+                { $pull : {
+                    comentarios : {
+                        id_comentario : objectId(req.params.id)
+                        }
+                    }
+                },
+                { multi : true },
                 function(err, records){
                     if(err){
                         res.json(err);
@@ -170,5 +175,4 @@ app.delete('/api/:id', function(req, res){
             );
         });
     });
-    */
 });
