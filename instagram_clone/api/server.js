a2ad05c1d14aa980f671f8704d88f15724ab8325
George Salu <george.salu10@gmail.com>
2018-03-02 22:32:31 -0300

    incluindo comentarios nas postagens 3

--- instagram_clone/api/server.js
+++ instagram_clone/api/server.js
@@ -126,13 +126,17 @@ app.get('/imagens/:imagem', function(req, res){
 
 // PUT by ID(update)
 app.put('/api/:id', function(req, res){
-    res.send(req.body.comentario);
-    /*
     db.open(function(err, mongoclient){
         mongoclient.collection('postagens', function(err, collection){
             collection.update(
                 { _id : objectId(req.params.id) },
-                { $set : { titulo : req.body.titulo }},
+                { $push : {
+                    comentarios : {
+                        id_comentario : new objectId(),
+                        comentario : req.body.comentario
+                        }
+                    }
+                },
                 {},
                 function(err, records){
                     if(err){
@@ -145,7 +149,6 @@ app.put('/api/:id', function(req, res){
             );
         });
     });
-    */
 });
 
 // DELETE by ID(delete)
